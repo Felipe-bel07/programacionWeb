dc93b04eb77ce6c7583ecba6b8e221bfa238e216
Felipe-bel07 <andresfbeltran07@gmail.com>
2026-04-27 04:15:32 -0500

    Advances hero and about

--- user_history_1/js/main.js
+++ user_history_1/js/main.js
@@ -5,3 +5,19 @@ function btnMenu(){
     menu[0].classList.toggle('nav--active')
 }
 
+
+const list = document.getElementById("skill__list")
+const skills = [
+"Java (OOP fundamentals)",
+"Basic Backend Development",
+"SQL & Databases",
+"Git & GitHub",
+"HTML5 & CSS3",
+];
+
+skills.forEach(skill =>{
+    const li = document.createElement("li");
+    li.classList.add("about__skills-item");
+    li.textContent = skill;
+    list.appendChild(li);
+})
